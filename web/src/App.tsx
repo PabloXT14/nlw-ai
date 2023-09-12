0e2a3f39270f372347183dc4d0f9db8e903d2d94
Pablo Alan <pabloxt14@gmail.com>
2023-09-12 20:18:27 -0300

    feat: created Sidebar section style

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,10 @@
-import { Github } from 'lucide-react';
+import { Github, FileVideo, Upload, Wand2 } from 'lucide-react';
 import { Button } from './components/ui/button';
 import { Separator } from './components/ui/separator';
 import { Textarea } from './components/ui/textarea';
+import { Label } from '@radix-ui/react-label';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './components/ui/select';
+import { Slider } from './components/ui/slider';
 
 export function App() {
   return (
@@ -43,6 +46,94 @@ export function App() {
           Lembre-se: você pode utilizar a variável <code className="text-violet-400">{`{transcription}`}</code> no seu prompt para adicionar o conteúdo da transcrição do vídeo selecionado.
           </p>
         </div>
+
+        {/* Sidebar */}
+        <aside className='w-80 space-y-6'>
+          <form className='space-y-6'>
+            <label
+              htmlFor="video"
+              className='border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5'
+            >
+              <FileVideo className='w-4 h-4' />
+              Selecione um vídeo
+            </label>
+
+            <input type="file" id="video" accept='video/mp4' className='sr-only' />
+
+            <Separator />
+
+            <div className='space-y-2'>
+              <Label htmlFor='transcription_prompt'>
+                Prompt de transcrição
+              </Label>
+              <Textarea
+                id='transcription_prompt'
+                placeholder='Inclua palavras-chave mencionadas no vídeo separadas por vírgula (,)'
+                className='h-20 leading-relaxed resize-none'
+              />
+            </div>
+
+            <Button type='submit' className='w-full'>
+              Carregar vídeo
+              <Upload className='w-4 h-4 ml-2' />
+            </Button>
+
+          </form>
+
+          <Separator />
+
+          <form className='space-y-6'>
+            <div className='space-y-2'>
+              <Label>Prompt</Label>
+              <Select>
+                <SelectTrigger>
+                  <SelectValue placeholder="Selecione um prompt..." />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="title">Título do YouTube</SelectItem>
+                  <SelectItem value="description">Descrição do YouTube</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+
+            <div className='space-y-2'>
+              <Label>Modelo</Label>
+              <Select disabled defaultValue='gpt3.5'>
+                <SelectTrigger>
+                  <SelectValue />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="gpt3.5">GPT 3.5-turbo 16k</SelectItem>
+                </SelectContent>
+              </Select>
+              <span className='block text-sm text-muted-foreground italic'>
+                Você poderá customizar essa opção em breve
+              </span>
+            </div>
+
+            <Separator />
+
+            <div className='space-y-4'>
+              <Label>Temperatura</Label>
+              <Slider
+                defaultValue={[0.5]}
+                min={0}
+                max={1}
+                step={0.1}
+              />
+              <span className='block text-sm text-muted-foreground italic leading-relaxed'>
+                Valores mais altor tendem a deixar o resultado mais criativo e com possíveis erros.
+              </span>
+            </div>
+
+            <Separator />
+
+            <Button type='submit' className='w-full'>
+              Executar
+              <Wand2 className='w-4 h-4 ml-2'/>
+            </Button>
+          </form>
+        </aside>
 
       </main>
     </div>
