5db1fddeac50e3c4362ab0a918413f06cba9c707
Pablo Alan <pabloxt14@gmail.com>
2023-09-12 19:02:58 -0300

    feat: create Header page style

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,27 @@
+import { Github } from 'lucide-react';
 import { Button } from './components/ui/button';
+import { Separator } from './components/ui/separator';
 
 export function App() {
   return (
     <div>
-      <Button>Click me</Button>
+      <div className='px-6 py-3 flex items-center justify-between border-b'>
+        <h1 className='text-xl font-bold'>upload.ai</h1>
+
+        <div className='flex items-center gap-3'>
+          <span className='text-sm text-muted-foreground'>
+            Desenvolvido com 💜 no NLW da Rocketseat
+          </span>
+
+          <Separator orientation='vertical' className='h-6' />
+
+          <Button variant='outline'>
+            <Github className='w-4 h-4 mr-2' />
+            Github
+          </Button>
+        </div>
+      </div>
+      
     </div>
   )
 }
