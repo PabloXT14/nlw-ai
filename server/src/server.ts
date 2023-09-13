41ed13c01c6b5fe2f342c9dbfcf7a193a93ae05c
Pablo Alan <pabloxt14@gmail.com>
2023-09-13 18:48:31 -0300

    feat: create videos route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,13 @@
 import { fastify } from 'fastify'
+import { getAllPromptsRoute } from './routes/get-all-prompts'
+import { uploadVideoRoute } from './routes/upload-video'
 
 const app = fastify()
 
 const PORT = 3333
 
-app.get('/', (request, reply) => {
-  reply.send('Hello World 🚀!')
-})
+app.register(getAllPromptsRoute)
+app.register(uploadVideoRoute)
 
 app.listen({
   port: PORT,
