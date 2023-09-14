702b8c1387f7d05f0871b02871dd41944f7c41d4
Pablo Alan <pabloxt14@gmail.com>
2023-09-14 00:11:10 -0300

    feat: created transcription route

---
+++ server/src/routes/create-transcription.ts
@@ -0,0 +1,52 @@
+import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import { prisma } from '../lib/prisma'
+import { createReadStream } from 'node:fs'
+import { openai } from '../lib/openai'
+
+export async function createTranscriptionRoute(app: FastifyInstance) {
+  app.post('/videos/:videoId/transcription', async (request, reply) => {
+    const paramsSchema = z.object({
+      videoId: z.string().uuid()
+    })
+
+    const { videoId } = paramsSchema.parse(request.params)
+
+    const bodySchema = z.object({
+      prompt: z.string()
+    })
+
+    const { prompt } = bodySchema.parse(request.body)
+
+    const video = await prisma.video.findUniqueOrThrow({
+      where: {
+        id: videoId,
+      }
+    })
+
+    const videoPath = video.path
+    const audioReadStream = createReadStream(videoPath)
+
+    const response = await openai.audio.transcriptions.create({
+      file: audioReadStream,
+      model: 'whisper-1',
+      language: 'pt',
+      response_format: 'json',
+      temperature: 0,
+      prompt,
+    })
+
+    const transcription = response.text
+
+    await prisma.video.update({
+      where: {
+        id: videoId,
+      },
+      data: {
+        transcription,
+      }
+    })
+
+    return reply.status(201).send({ transcription })
+  })
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import { fastify } from 'fastify'
 import { getAllPromptsRoute } from './routes/get-all-prompts'
 import { uploadVideoRoute } from './routes/upload-video'
+import { createTranscriptionRoute } from './routes/create-transcription'
 
 const app = fastify()
 
@@ -8,6 +9,7 @@ const PORT = 3333
 
 app.register(getAllPromptsRoute)
 app.register(uploadVideoRoute)
+app.register(createTranscriptionRoute)
 
 app.listen({
   port: PORT,
