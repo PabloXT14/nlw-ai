65a3c9cca0162c4cec42f58b7aa1473f16c60dbc
Pablo Alan <pabloxt14@gmail.com>
2023-09-15 17:50:00 -0300

    feat: created logic to generate video title or description

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,6 +6,7 @@ import { Textarea } from './components/ui/textarea';
 import { Label } from '@radix-ui/react-label';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './components/ui/select';
 import { Slider } from './components/ui/slider';
+import { useCompletion } from 'ai/react'
 
 import { VideoInputForm } from './components/video-inpuit-form';
 import { PromptSelect } from './components/prompt-select';
@@ -14,9 +15,23 @@ export function App() {
   const [temperature, setTemperature] = useState(0.5)
   const [videoId, setVideoId] = useState<string | null>(null)
 
-  function handlePromptSelected(template: string) {
-    console.log(template)
-  }
+  const {
+    input,
+    setInput,
+    handleInputChange,
+    handleSubmit,
+    completion,
+    isLoading
+  } = useCompletion({
+    api: 'http://localhost:3333/ai/complete',
+    body: {
+      videoId,
+      temperature,
+    },
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  })
 
   return (
     <div className='min-h-screen flex flex-col'>
@@ -45,11 +60,14 @@ export function App() {
             <Textarea 
               className='resize-none p-4 leading-relaxed'
               placeholder='Inclua o prompt para a IA...'
+              value={input}
+              onChange={handleInputChange}
             />
             <Textarea 
               className='resize-none p-4 leading-relaxed'
               placeholder='Resultado gerado pela IA'
               readOnly
+              value={completion}
             />
           </div>
 
@@ -64,10 +82,10 @@ export function App() {
 
           <Separator />
 
-          <form className='space-y-6'>
+          <form onSubmit={handleSubmit} className='space-y-6'>
             <div className='space-y-2'>
               <Label>Prompt</Label>
-              <PromptSelect onPromptSelected={handlePromptSelected} />
+              <PromptSelect onPromptSelected={setInput} />
             </div>
 
             <div className='space-y-2'>
@@ -103,7 +121,7 @@ export function App() {
 
             <Separator />
 
-            <Button type='submit' className='w-full'>
+            <Button disabled={isLoading} type='submit' className='w-full'>
               Executar
               <Wand2 className='w-4 h-4 ml-2'/>
             </Button>
